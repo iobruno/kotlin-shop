9df0875ee55ef507477c2a47e923e8c3e7edb8b7
Bruno <bruno.ariev@gmail.com>
2019-09-26 13:28:59 -0300

    incl. validations on Account model

--- src/main/kotlin/io/petproject/model/Account.kt
+++ src/main/kotlin/io/petproject/model/Account.kt
@@ -1,5 +1,12 @@
 package io.petproject.model
 
+import io.petproject.utils.Patterns.emailRegex
+
 data class Account(val name: String, val email: String) {
+
+    init {
+        require(name.isNotEmpty()) { "Name cannot be blank" }
+        require(emailRegex.matcher(email).matches()) { "Invalid email address" }
+    }
 
 }
