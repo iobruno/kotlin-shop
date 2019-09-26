7551f7f0dff94c27d902e801155e387c98ed5c1d
Bruno <bruno.ariev@gmail.com>
2019-09-26 15:54:44 -0300

    setting up interface for PaymentMethod and incl. CreditCard implementation

--- src/main/kotlin/io/petproject/model/PaymentMethod.kt
+++ src/main/kotlin/io/petproject/model/PaymentMethod.kt
@@ -1,5 +1,23 @@
 package io.petproject.model
 
+import java.math.BigDecimal
+import java.time.YearMonth
+
 interface PaymentMethod {
+
+    val billingAddress: Address
+
+    fun charge(amount: BigDecimal): Boolean
+}
+
+data class CreditCard(val nameOnCard: String,
+                      val number: String,
+                      val securityCode: Int,
+                      val expiresAt: YearMonth,
+                      override val billingAddress: Address) : PaymentMethod {
+
+    override fun charge(amount: BigDecimal): Boolean {
+        TODO("attempt transaction with payment broker")
+    }
 
 }
