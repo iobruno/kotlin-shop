1cff9ec0f9cdc16d5489fd0d272c14060c35a4d0
Bruno <bruno.ariev@gmail.com>
2019-09-26 13:28:43 -0300

    incl. Patterns for email regex

---
+++ src/main/kotlin/io/petproject/utils/Patterns.kt
@@ -0,0 +1,16 @@
+package io.petproject.utils
+
+import java.util.regex.Pattern
+
+object Patterns {
+
+    val emailRegex: Pattern = Pattern.compile(
+            "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+                    "\\@" +
+                    "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                    "(" +
+                    "\\." +
+                    "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+                    ")+"
+    )
+}
