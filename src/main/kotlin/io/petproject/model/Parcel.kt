f8896692610eef5a1161bf677ce28e68425862c8
Bruno <bruno.ariev@gmail.com>
2019-09-26 17:31:02 -0300

    incl. mock methods to compute shipping costs

--- src/main/kotlin/io/petproject/model/Parcel.kt
+++ src/main/kotlin/io/petproject/model/Parcel.kt
@@ -1,5 +1,8 @@
 package io.petproject.model
 
+import java.math.BigDecimal
+import java.math.RoundingMode
+
 data class Parcel(val items: List<Item>,
                   val shippingAddress: Address,
                   val shippingLabel: ShippingLabel) {
@@ -9,6 +12,28 @@ data class Parcel(val items: List<Item>,
         fun of(items: List<Item>, shippingAddress: Address): List<Parcel> {
             TODO("implement parsing of items into parcels")
         }
+
+        fun shippingCostsOf(parcels: List<Parcel>): BigDecimal {
+            return parcels.map { it.shippingAndHandlingCosts() }
+                    .fold(BigDecimal.ZERO) { acc, value -> acc.plus(value) }
+                    .setScale(2, RoundingMode.HALF_UP)
+        }
+
+        fun importationFeesOf(parcels: List<Parcel>): BigDecimal {
+            return parcels.map { it.importationTaxesAndFees() }
+                    .fold(BigDecimal.ZERO) { acc, value -> acc.plus(value) }
+                    .setScale(2, RoundingMode.HALF_UP)
+        }
+    }
+
+    fun shippingAndHandlingCosts(): BigDecimal {
+        //TODO("compute logistics costs based on size, weight and shippingAddress")
+        return BigDecimal.TEN
+    }
+
+    fun importationTaxesAndFees(): BigDecimal {
+        //TODO("compute taxes for importation in the destination country")
+        return BigDecimal.ZERO
     }
 
 }
