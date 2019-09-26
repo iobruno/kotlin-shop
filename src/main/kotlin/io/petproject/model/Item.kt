b4df9b67b5dbf6c688e38374a07b754fe33a5e7c
Bruno <bruno.ariev@gmail.com>
2019-09-26 13:58:25 -0300

    incl. addMore() and updateQuantity() on Item

--- src/main/kotlin/io/petproject/model/Item.kt
+++ src/main/kotlin/io/petproject/model/Item.kt
@@ -1,10 +1,21 @@
 package io.petproject.model
 
-data class Item(val product: Product, val quantity: Int) {
+data class Item(val product: Product, var quantity: Int) {
 
     val subtotal = product.price.times(quantity.toBigDecimal())
 
     init {
         require(quantity > 0) { "Quantity must be greaterThan 0" }
     }
+
+    fun addMore(quantity: Int) = apply {
+        require(quantity > 0) { "Quantity must be greaterThan 0" }
+        this.quantity += quantity
+    }
+
+    fun updateTo(quantity: Int) = apply {
+        require(quantity >= 0) { "Quantity must be equalTo or greaterThan 0" }
+        this.quantity = quantity
+    }
+
 }
