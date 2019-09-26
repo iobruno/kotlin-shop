6e2d23e0dd916e50e34efe6af06fe722581da294
Bruno <bruno.ariev@gmail.com>
2019-09-26 14:56:52 -0300

    breaking down Order in Physical, Digital and SubcriptionOrders

--- src/main/kotlin/io/petproject/model/Order.kt
+++ src/main/kotlin/io/petproject/model/Order.kt
@@ -1,5 +1,37 @@
 package io.petproject.model
 
-class Order {
+interface Order {
+    val type: OrderType
+    val items: List<Item>
+    val account: Account
+}
 
+data class PhysicalOrder(override val items: List<Item>,
+                         override val account: Account) : Order {
+
+    override val type = OrderType.PHYSICAL
+
+
+}
+
+data class DigitalOrder(override val items: List<Item>,
+                        override val account: Account) : Order {
+
+    override val type = OrderType.DIGITAL
+
+}
+
+data class SubscriptionOrder(override val items: List<Item>,
+                             override val account: Account) : Order {
+
+    constructor(item: Item, account: Account): this(listOf(item), account)
+
+    override val type = OrderType.SUBSCRIPTION
+
+}
+
+enum class OrderType {
+    PHYSICAL,
+    DIGITAL,
+    SUBSCRIPTION
 }
