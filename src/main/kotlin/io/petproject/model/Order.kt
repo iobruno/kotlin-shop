dff33795216a6b42f9e11be801562b58c2433c18
Bruno <bruno.ariev@gmail.com>
2019-09-26 15:46:43 -0300

    adding OrderStatus to handle state control

--- src/main/kotlin/io/petproject/model/Order.kt
+++ src/main/kotlin/io/petproject/model/Order.kt
@@ -90,3 +90,16 @@ enum class OrderType {
     DIGITAL,
     SUBSCRIPTION
 }
+
+enum class OrderStatus(val code: Int = 0) {
+    UNKNOWN,
+    PENDING(100),
+    NOT_SHIPPED(200),
+    UNSENT(200),
+    PENDING_ACTIVATION(200),
+    SHIPPED(300),
+    SENT(300),
+    DELIVERED(400),
+    REDEEMED(400),
+    ACTIVATED(400)
+}
