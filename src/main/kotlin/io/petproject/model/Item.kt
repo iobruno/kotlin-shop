bde19ee74ce94ab27ed146de1b97b847739b8b3a
Bruno <bruno.ariev@gmail.com>
2019-09-26 13:14:32 -0300

    incl. validations on Product and Item

--- src/main/kotlin/io/petproject/model/Item.kt
+++ src/main/kotlin/io/petproject/model/Item.kt
@@ -3,4 +3,8 @@ package io.petproject.model
 data class Item(val product: Product, val quantity: Int) {
 
     val subtotal = product.price.times(quantity.toBigDecimal())
+
+    init {
+        require(quantity > 0) { "Quantity must be greaterThan 0" }
+    }
 }
--- src/main/kotlin/io/petproject/model/Product.kt
+++ src/main/kotlin/io/petproject/model/Product.kt
@@ -1,11 +1,32 @@
 package io.petproject.model
 
+import java.lang.ClassCastException
 import java.math.BigDecimal
+import java.math.RoundingMode
 
 data class Product(val name: String, val type: ProductType, private val _price: Double) {
 
-    val price: BigDecimal = BigDecimal(_price)
+    val price: BigDecimal = BigDecimal(_price).setScale(2, RoundingMode.HALF_UP)
 
+    init {
+        require(name.isNotBlank()) { "Product name must not be blank" }
+        require(_price > 0.0) { "Produce price must be greaterThan 0" }
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return try {
+            val that = other as Product
+            (this.name == that.name) && (this.type == that.type)
+        } catch (ex: ClassCastException) {
+            false
+        }
+    }
+
+    override fun hashCode(): Int {
+        var result = name.hashCode()
+        result = 31 * result + type.hashCode()
+        return result
+    }
 }
 
 enum class ProductType {
