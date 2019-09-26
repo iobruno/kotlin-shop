2c71c575dfb7b3b1dd5b89143f61cbc14398569b
Bruno <bruno.ariev@gmail.com>
2019-09-26 15:32:20 -0300

    scaffolding methods to place() and pay() for an order

--- src/main/kotlin/io/petproject/model/Order.kt
+++ src/main/kotlin/io/petproject/model/Order.kt
@@ -1,25 +1,59 @@
 package io.petproject.model
 
+
 interface Order {
-    val type: OrderType
     val items: List<Item>
     val account: Account
+    var paymentMethod: PaymentMethod
+    val type: OrderType
+
+    fun withPaymentMethod(paymentMethod: PaymentMethod): Order
+
+    fun place(): Order
+
+    fun pay(): Order
 }
 
 data class PhysicalOrder(override val items: List<Item>,
                          override val account: Account) : Order {
 
     override val type = OrderType.PHYSICAL
+    override lateinit var paymentMethod: PaymentMethod
 
+    override fun withPaymentMethod(paymentMethod: PaymentMethod) = apply {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun place() = apply {
+        require(this::paymentMethod.isInitialized)
+    }
+
+    override fun pay() = apply {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
 }
+
 
 data class DigitalOrder(override val items: List<Item>,
                         override val account: Account) : Order {
 
     override val type = OrderType.DIGITAL
+    override lateinit var paymentMethod: PaymentMethod
 
+    override fun withPaymentMethod(paymentMethod: PaymentMethod) = apply {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun place() = apply {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun pay() = apply {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
+
 
 data class SubscriptionOrder(override val items: List<Item>,
                              override val account: Account) : Order {
@@ -27,7 +61,19 @@ data class SubscriptionOrder(override val items: List<Item>,
     constructor(item: Item, account: Account): this(listOf(item), account)
 
     override val type = OrderType.SUBSCRIPTION
+    override lateinit var paymentMethod: PaymentMethod
 
+    override fun withPaymentMethod(paymentMethod: PaymentMethod): Order {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun place() = apply {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun pay() = apply {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
 
 enum class OrderType {
