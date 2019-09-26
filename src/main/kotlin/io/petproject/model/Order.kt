1508ddf0a208d6f74cde81dc7e01697c05c31a7e
Bruno <bruno.ariev@gmail.com>
2019-09-26 15:40:42 -0300

    incl. validations for payment and shippingAddress for Physical Order

--- src/main/kotlin/io/petproject/model/Order.kt
+++ src/main/kotlin/io/petproject/model/Order.kt
@@ -7,7 +7,9 @@ interface Order {
     var paymentMethod: PaymentMethod
     val type: OrderType
 
-    fun withPaymentMethod(paymentMethod: PaymentMethod): Order
+    fun withPaymentMethod(paymentMethod: PaymentMethod) = apply {
+        this.paymentMethod = paymentMethod
+    }
 
     fun place(): Order
 
@@ -20,11 +22,18 @@ data class PhysicalOrder(override val items: List<Item>,
     override val type = OrderType.PHYSICAL
     override lateinit var paymentMethod: PaymentMethod
 
+    lateinit var shippingAddress: Address
+
+    fun withShippingAddress(address: Address) = apply {
+        this.shippingAddress = address
+    }
+
     override fun withPaymentMethod(paymentMethod: PaymentMethod) = apply {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        super.withPaymentMethod(paymentMethod)
     }
 
     override fun place() = apply {
+        require(this::shippingAddress.isInitialized)
         require(this::paymentMethod.isInitialized)
     }
 
@@ -42,11 +51,11 @@ data class DigitalOrder(override val items: List<Item>,
     override lateinit var paymentMethod: PaymentMethod
 
     override fun withPaymentMethod(paymentMethod: PaymentMethod) = apply {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        super.withPaymentMethod(paymentMethod)
     }
 
     override fun place() = apply {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        require(this::paymentMethod.isInitialized)
     }
 
     override fun pay() = apply {
@@ -63,12 +72,12 @@ data class SubscriptionOrder(override val items: List<Item>,
     override val type = OrderType.SUBSCRIPTION
     override lateinit var paymentMethod: PaymentMethod
 
-    override fun withPaymentMethod(paymentMethod: PaymentMethod): Order {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun withPaymentMethod(paymentMethod: PaymentMethod) = apply {
+        super.withPaymentMethod(paymentMethod)
     }
 
     override fun place() = apply {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        require(this::paymentMethod.isInitialized)
     }
 
     override fun pay() = apply {
