8b47308cf8f317ea347d5995cac438aa8228a867
Bruno <bruno.ariev@gmail.com>
2019-09-26 14:02:22 -0300

    implementing add, updateQuantity an delete

--- src/main/kotlin/io/petproject/model/ShoppingCart.kt
+++ src/main/kotlin/io/petproject/model/ShoppingCart.kt
@@ -1,20 +1,46 @@
 package io.petproject.model
 
+import java.lang.IllegalArgumentException
 import java.math.BigDecimal
+import java.math.RoundingMode
+import java.util.function.BiFunction
 
 class ShoppingCart {
 
     val items = HashMap<Product, Item>()
 
     val subtotal: () -> BigDecimal = {
-        TODO("implement computation of subtotal")
+        items.values.asSequence()
+                .map { item -> item.subtotal }
+                .fold(BigDecimal.ZERO) { acc, value ->acc.plus(value) }
+                .setScale(2, RoundingMode.HALF_UP)
     }
 
-    fun add(product: Product, quantity: Int) = apply { }
+    fun add(product: Product, quantity: Int) = apply {
+        items.compute(product, BiFunction { _, item ->
+            item?.addMore(quantity) ?:
+            Item(product, quantity)
+        })
+    }
 
-    fun updateQuantity(product: Product, quantity: Int) = apply { }
+    fun updateQuantity(product: Product, quantity: Int) = apply {
+        if (quantity == 0) {
+            delete(product)
+        } else {
+            items.compute(product, BiFunction { _, item ->
+                item?.updateTo(quantity) ?:
+                throw IllegalArgumentException("Product specified is not in the Cart")
+            })
+        }
+    }
 
-    fun delete(product: Product) = apply { }
+    fun delete(product: Product) = apply {
+        if (items.containsKey(product)) {
+            items.remove(product)
+        } else {
+            throw IllegalArgumentException("Product specified is not in the Cart")
+        }
+    }
 
     fun checkout(account: Account): List<Order> {
         TODO("implement checkout for a variety of items in the cart")
