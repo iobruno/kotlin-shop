6744f43b201da1a8b3e7284c760e3db59d6854d2
iobruno <bruno.ariev@gmail.com>
2020-10-07 03:21:10 -0300

    refactoring accountTests with assertThatThrownBy

--- src/test/kotlin/io/petproject/model/AccountTest.kt
+++ src/test/kotlin/io/petproject/model/AccountTest.kt
@@ -1,25 +1,21 @@
 package io.petproject.model
 
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Assertions.*
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 
 internal class AccountTest {
 
     @Test
     fun `when name is blank, throw IllegalArgEx`() {
-        val ex = assertThrows(IllegalArgumentException::class.java) {
-            Account("", "john.doe@domain.suffix")
-        }
-        assertThat(ex.message).isEqualTo("Name cannot be blank")
+        assertThatThrownBy { Account("", "john.doe@domain.suffix") }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("Name cannot be blank")
     }
 
     @Test
     fun `when email is invalid, throw IllegalArgEx`() {
-        val ex = assertThrows(IllegalArgumentException::class.java) {
-            Account("Bruno", "invalidEmail")
-        }
-        assertThat(ex.message).isEqualTo("Invalid email address")
+        assertThatThrownBy { Account("Bruno", "invalidEmail") }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("Invalid email address")
     }
-
 }
